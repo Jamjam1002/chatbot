3aece62d4478dade01b141b1d6b1ee27c221f077
Eldhe Mheyr G. Magadan <135022016+Jamjam1002@users.noreply.github.com>
2025-03-10 13:47:07 +0800

    Update app.py

--- ronnierecommender_december/app.py
+++ ronnierecommender_december/app.py
@@ -2,6 +2,8 @@
 # Copyright (C) 2024 Ronnie Perolino
 
 import streamlit as st
+import sys
+import os
 from chat_api_handler import ChatAPIHandler
 from streamlit_mic_recorder import mic_recorder
 from utils import get_timestamp, load_config, get_avatar
@@ -11,6 +13,17 @@ from html_templates import css
 from database_operations import save_text_message, save_image_message, save_audio_message, load_messages, get_all_chat_history_ids, delete_chat_history, load_last_k_text_messages_ollama
 from utils import list_openai_models, list_ollama_models, command
 import sqlite3
+
+# Force Streamlit Cloud to use pysqlite3
+try:
+    import pysqlite3
+    sys.modules["sqlite3"] = pysqlite3
+except ImportError:
+    print("pysqlite3 not found. Falling back to default sqlite3.")
+
+import sqlite3
+print(f"SQLite version in use: {sqlite3.sqlite_version}")
+
 
 # Load configuration
 config = load_config()
